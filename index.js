63e0383dbc6b4853e22dc86ec0c16293f765d39e
kaiumuddin <kaium.uddin2909@gmail.com>
2022-11-30 22:04:05 +0600

    Env done

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const uri = "mongodb+srv://mobileWizardUser:qFyWTEg2fLKImH5P@cluster0.rmad3ac.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rmad3ac.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
 async function run() {
     try {
